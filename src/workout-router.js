b482504ca0130078f743a4f74f864d8f21a3322a
Oscar Zamudio <zamudio_1526@hotmail.com>
2020-05-21 19:45:05 -0700

    updates

--- src/workout-router.js
+++ src/workout-router.js
@@ -1,24 +1,24 @@
 const express = require('express');
-// const WorkoutService = require('./workout-service');
+const WorkoutService = require('./workout-service');
 
 const workoutRouter = express.Router();
 
 const serializeWorkout = workout => ({
-    id: id,
-  bodypart: '',
-  name: '',
-  video: '',
-  desc: '',
+    id: workout.id,
+  bodypart: workout.bodypart,
+  workout_name: workout.workout_name,
+  workout_video: workout.workout_video,
+  workout_desc: workout.workout_desc,
 })
 
 workoutRouter
 .route('/').get((req, res, next) => {
-    // const knexInstance = req.app.get('db')
-    // .then(workout => {
-    //     console.log(workout)
-    // })
-    // .catch(next)
-    res.send({workout: true})
-})
+    const knexInstance = req.app.get('db')
+    WorkoutService.getAllWorkouts(knexInstance)
+    .then(workout => {
+        res.json(workout.map(serializeWorkout))
+    })
+    .catch(next)
+});
 
 module.exports = workoutRouter;
